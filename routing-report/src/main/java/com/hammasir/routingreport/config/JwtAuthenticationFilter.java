262405109546e93f0717ead721dc1821f066b401
mahtab.mahdavi <mahtaabmahdavi@gmail.com>
2023-09-14 22:23:26 +0330

    implement jwt service

--- routing-report/src/main/java/com/hammasir/routingreport/config/JwtAuthenticationFilter.java
+++ routing-report/src/main/java/com/hammasir/routingreport/config/JwtAuthenticationFilter.java
@@ -13,13 +13,27 @@ import java.io.IOException;
 
 @Component
 @RequiredArgsConstructor
-public class JWTAuthenticationFilter extends OncePerRequestFilter {
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+
+    public JwtAuthenticationFilter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
-
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String username;
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwt = authHeader.substring(7);
+        username = jwtService.extractUsername(jwt);
     }
 }
